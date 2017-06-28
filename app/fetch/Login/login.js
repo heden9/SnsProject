ef42214ec45d65269692558f16137b58f5d40031
mangx <771854332@qq.com>
2017-06-28 18:00:25 +0800

    ++++

--- app/fetch/Login/login.js
+++ app/fetch/Login/login.js
@@ -7,10 +7,3 @@ export function login(data) {
     const result = post('http://localhost:3000/checkLogin', data);
     return result;
 }
-
-export function ppp(data) {
-    if(data == null)
-        return;
-    const result = post('http://localhost:3000/login');
-    return result;
-}
